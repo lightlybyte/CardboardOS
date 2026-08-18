7c4874e7dddbfa80c128fe96562ba1ebf7c7adbf
LightlyByte <lightlybyte@gmail.com>
2026-08-18 14:14:14 +0100

    Initial Commit #27: Fixing Kernel Bug With Assembly Code In It

--- src/kmain.c
+++ src/kmain.c
@@ -1,44 +1,4 @@
-; src/boot.asm - Multiboot compliant bootloader
-; Build: nasm -f elf32 src/boot.asm -o build/boot.o
-
-[BITS 32]
-[GLOBAL start]
-[EXTERN kmain]      ; External C function
-
-; Multiboot header - GRUB 0.95 compatible
-section .multiboot
-align 4
-    dd 0x1BADB002          ; Magic number
-    dd 0x03                ; Flags: align on 4KB, provide memory info
-    dd -(0x1BADB002 + 0x03) ; Checksum
-
-; Kernel code section
-section .text
-start:
-    ; Set up stack pointer (16KB stack)
-    mov esp, stack_end
-    
-    ; Clear EFLAGS
-    push 0
-    popf
-    
-    ; Save multiboot info pointer for C code
-    push ebx
-    
-    ; Call kmain
-    call kmain
-    
-    ; If kmain returns, hang
-    cli
-    hlt
-    jmp $
-
-; Stack section - 16KB stack
-section .bss
-align 16
-stack_bottom:
-    resb 16384  ; 16KB stack
-stack_end:// src/kmain.c - Simple kernel with BRASH shell
+// src/kmain.c - Simple kernel with BRASH shell
 #define VIDEO_MEMORY 0xB8000
 #define VGA_WIDTH 80
 #define VGA_HEIGHT 25
