143aa9042c3dfc050cc5267535a1429167aebfda
LightlyByte <lightlybyte@gmail.com>
2026-09-16 00:27:12 +0100

    Fixed Build Script and New Gitignore so I do not have to keep on deleting files

--- build.py
+++ build.py
@@ -1,21 +1,54 @@
 import os
+import shutil
 
-kernellocation = os.path.join(os.getcwd(), "src", "kernel")
-kernelfile = os.path.join(kernellocation, "kmain.c")
-multibootfile = os.path.join(kernellocation, "boot.asm")
-linkerfile = os.path.join(kernellocation, "linker.ld")
-buildlocation = os.path.join(os.getcwd(), "build")
+root           = os.getcwd()
+kernel_dir     = os.path.join(root, "src", "kernel")
+build_dir      = os.path.join(root, "build")
+iso_dir        = os.path.join(build_dir, "iso")
+grub_dir       = os.path.join(iso_dir, "boot", "grub")
+
+kmain_src      = os.path.join(kernel_dir, "kmain.c")
+boot_src       = os.path.join(kernel_dir, "boot.asm")
+linker_script  = os.path.join(kernel_dir, "linker.ld")
+
+boot_obj       = os.path.join(build_dir, "boot.o")
+kmain_obj      = os.path.join(build_dir, "kmain.o")
+kernel_elf     = os.path.join(build_dir, "cardboard.elf")
+iso_path       = os.path.join(build_dir, "cardboard.iso")
+
+os.makedirs(build_dir, exist_ok=True)
+os.makedirs(grub_dir, exist_ok=True)
 
 print("[ 1/3 ] Compiling OS Kernel...")
-os.system(f"nasm -f elf32 {multibootfile} -o {buildlocation}/boot.o")
-os.system(f"clang -target i386-elf -m32 -ffreestanding -fno-stack-protector -fno-builtin -c {kernelfile} -o {buildlocation}/kmain.o")
-os.system(f"ld.lld -m elf_i386 -T {linkerfile} {buildlocation}/boot.o {buildlocation}/kmain.o -o {buildlocation}/cardboard.elf")
+os.system(f'nasm -f elf32 "{boot_src}" -o "{boot_obj}"')
+os.system(f'clang -target i386-elf -m32 -ffreestanding -fno-stack-protector '
+          f'-fno-builtin -c "{kmain_src}" -o "{kmain_obj}"')
+os.system(f'ld.lld -m elf_i386 -T "{linker_script}" "{boot_obj}" "{kmain_obj}" '
+          f'-o "{kernel_elf}"')
 
-print("[ 2/3 ] Creating ISO Image... (For GRUB 0.97)")
-os.system(f"mkdir -p {buildlocation}/iso/boot/grub")
-os.system(f"cp {buildlocation}/cardboard.elf {buildlocation}/iso/boot/cardboard.elf")
-os.system(f"echo 'set timeout=0\nset default=0\nmenuentry \"CardboardOS\" {{\n\tmultiboot /boot/cardboard.elf\n\tboot\n}}' > {buildlocation}/iso/boot/grub/grub.cfg")
-os.system(f"mkisofs -R -b boot/grub/stage2_eltorito -no-emul-boot -boot-load-size 4 -A \"CardboardOS\" -input-charset utf8 -o {buildlocation}/cardboard.iso {buildlocation}/iso")
+print("[ 2/3 ] Creating ISO Image...")
+shutil.copy(kernel_elf, os.path.join(iso_dir, "boot", "cardboard.elf"))
+
+# menu.lst for GRUB 0.97 (NOT grub.cfg)
+menu_lst = (
+    "default 0\n"
+    "timeout 0\n"
+    "title CardboardOS\n"
+    "root (cd)\n"
+    "kernel /boot/cardboard.elf\n"
+)
+with open(os.path.join(grub_dir, "menu.lst"), "w") as f:
+    f.write(menu_lst)
+
+# You must supply stage2_eltorito from your GRUB 0.97 install.
+stage2_src = os.path.join(kernel_dir, "stage2_eltorito")
+stage2_dst = os.path.join(grub_dir, "stage2_eltorito")
+shutil.copy(stage2_src, stage2_dst)
+
+os.system(
+    f'mkisofs -R -b boot/grub/stage2_eltorito -no-emul-boot '
+    f'-boot-load-size 4 -A "CardboardOS" -o "{iso_path}" "{iso_dir}"'
+)
 
 print("[ 3/3 ] Running OS in QEMU...")
-os.system(f"qemu-system-i386 -cdrom {buildlocation}/cardboard.iso")
+os.system(f'qemu-system-i386 -cdrom "{iso_path}"')
